d29c0dbefbf18bca3efed7126ff613de9e8a06b5
Wilson Hung <wilsonhung1@gmail.com>
2015-11-04 13:11:13 -0600

    setup git

---
+++ viewers/imgurviewer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+class ImgurViewer:
+    'Imgur Viewer Static Class'
+
+    @staticmethod
+    def request_pin():
+        print("Please click the following link to validate imgur account login:")
